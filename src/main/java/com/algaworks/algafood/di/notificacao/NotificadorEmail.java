19461ad3ae257322c51af8c390fa6a513fe6c33c
Lucas Souza <lucas-machado@outlook.com>
2020-09-22 20:45:14 +0100

    Using @Value to inject properties value to objects in spring beans from application.properties file.

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.annotation.TipoDoNotificador;
 import com.algaworks.algafood.di.enumeration.NivelUrgencia;
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +11,18 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmail implements Notificador {
+
+	@Value("${notificador.email.host-server}")
+	private String host;
+
+	@Value("${notificador.email.port-server}")
+	private Integer port;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Hostname: " + this.host);
+		System.out.println("Port: " + this.port);
+
 		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(),  mensagem);
 	}
