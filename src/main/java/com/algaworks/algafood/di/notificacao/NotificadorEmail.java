d66f0e45bd497c74b3b1faaef2e82bb5e30bf725
Lucas Souza <lucas-machado@outlook.com>
2020-09-23 10:00:03 +0100

    Using @ConfigurationProperties to inject properties value to a separated spring bean. It will let the code cleaner and well organized ti future maintenance.

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -3,7 +3,8 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.annotation.TipoDoNotificador;
 import com.algaworks.algafood.di.enumeration.NivelUrgencia;
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Value;
+import com.algaworks.algafood.di.properties.NotificacaoProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
-	@Value("${notificador.email.host-server}")
-	private String host;
-
-	@Value("${notificador.email.port-server}")
-	private Integer port;
+	@Autowired
+	private NotificacaoProperties properties;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Hostname: " + this.host);
-		System.out.println("Port: " + this.port);
+		System.out.println("Hostname: " + properties.getHostServer());
+		System.out.println("Port: " + properties.getPortServer());
 
 		System.out.printf("Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(),  mensagem);
