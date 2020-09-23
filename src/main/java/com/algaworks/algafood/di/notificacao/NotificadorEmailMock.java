798fdd0edf80ea12dfd718b2127372c109c9b875
Lucas Souza <lucas-machado@outlook.com>
2020-09-23 10:50:37 +0100

    Using application.properties to define a profile and run the application following the specified profile, dev or prod. Doing that will change the configurations properties injected at NotificationProperties bean.

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.di.notificacao;
 import com.algaworks.algafood.di.annotation.TipoDoNotificador;
 import com.algaworks.algafood.di.enumeration.NivelUrgencia;
 import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.properties.NotificacaoProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +12,15 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmailMock implements Notificador {
-	
+
+	@Autowired
+	private NotificacaoProperties properties;
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
+		System.out.println("Hostname: " + properties.getHostServer());
+		System.out.println("Port: " + properties.getPortServer());
+
 		System.out.printf("MOCK: Notificando %s através do e-mail %s: %s\n",
 				cliente.getNome(), cliente.getEmail(),  mensagem);
 	}
